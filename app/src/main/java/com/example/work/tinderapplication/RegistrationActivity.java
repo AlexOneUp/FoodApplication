06184fc253d7f54bb581c9d140e5cdbc579cd261
AlexOneUp <ruanalex11235@gmail.com>
2018-09-13 00:53:01 -0400

    Created User authentication on the registration page. Connected the database to the application

--- app/src/main/java/com/example/work/tinderapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/work/tinderapplication/RegistrationActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,11 +17,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button mRegister;
-    private EditText mEmail, mPassword;
+    private EditText mEmail, mPassword, mName;
+
+//    Private class Radiogroup to listen for the users input in activity_registration.xml
+//    Private class available to this file context
+    private RadioGroup mRadioGroup;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
@@ -49,16 +57,50 @@ public class RegistrationActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
 
+        mName = (EditText) findViewById(R.id.name);
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                variable selectId gets the selected RadioGroup which gets the radio button.
+                int selectId = mRadioGroup.getCheckedRadioButtonId();
+//                gets the associated result radioButton within the RadioGroup input by the user
+                final RadioButton radioButton = (RadioButton) findViewById(selectId);
+
+//                user must choose something to register, otherwise it won't work.
+//                check IF the user has chosen something
+                if (radioButton.getText() == null){
+
+                    return;
+//               The setOnClickListener will end here and will not create a new user. This authenticates existing users from non-existing ones
+
+                }
+
+
+//                activity_registration.xml inputs here are turned into a string
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
+                final String name = mName.getText().toString();
+
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+//                      We want to save USER INFORMATION to our DATABASE IF the task is successful
                         if(!task.isSuccessful()){
                             Toast.makeText(RegistrationActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
+                        } else {
+//                          Otherwise, we get the current user in our Database called "Users", "Male"/"Female", "Uid"
+//                          Database reference
+
+                            String userId = mAuth.getCurrentUser().getUid();
+//                            Database query to get the user input and register it to the database whether the user is male or female. This is possible because of getReference() pointing to our
+//                            database. We are chaining these commands .child of the database to point to Male or Female which radioButton is the user input and we create a new user this way.
+                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(radioButton.getText().toString()).child(userId).child("name");
+                            currentUserDb.setValue(name);
                         }
                     }
                 });
